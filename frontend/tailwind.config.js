e79948da6b963a75ff1d24924253793cef402114
Shetu Srivastav <srivastavashetu@gmail.com>
2025-05-05 18:28:36 +0530

    Update Tailwind, PostCSS config and dependencies

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,11 @@
-// tailwind.config.js
-
 module.exports = {
-    content: [
-      "./src/**/*.{html,js,jsx,ts,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  };
-
-  
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
