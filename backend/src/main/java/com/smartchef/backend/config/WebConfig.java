14d53cc7fffc02db61d7b1e1d4f67cc38371d41b
Shetu Srivastav <136826438+Shetu003@users.noreply.github.com>
2025-05-06 00:18:30 +0530

    WebConfig.java

--- backend/src/main/java/com/smartchef/backend/config/WebConfig.java
+++ backend/src/main/java/com/smartchef/backend/config/WebConfig.java
@@ -3,17 +3,13 @@ package com.smartchef.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
-                        "http://localhost:5173",
-                        "https://smart-chef-ai.vercel.app" 
-                )
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*") // only for testing
+                .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
