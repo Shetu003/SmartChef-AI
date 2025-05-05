20aa0c8d0872022f884c8d18703ff64ca91f1951
Shetu Srivastav <srivastavashetu@gmail.com>
2025-05-05 17:50:09 +0530

    Added .env file and updated frontend proxy configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,12 +8,12 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': apiUrl, // Proxy to the backend API
+      '/api': apiUrl,  // Proxy to the backend API
     },
   },
-  root: '.', // Set root directory to the current folder where `index.html` and `vite.config.js` are located
+  root: '.',  // Set root directory to the current folder where `index.html` and `vite.config.js` are located
   build: {
-    outDir: './build', // Output build files to `build` folder
-    emptyOutDir: true, // Clean the `build` folder before building
+    outDir: './build',  // Output build files to `build` folder
+    emptyOutDir: true,  // Clean the `build` folder before building
   },
 })
